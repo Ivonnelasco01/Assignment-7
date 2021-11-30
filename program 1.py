7da20c6995ec1dadac92e7b324d599782c216c2c
Jaren <ivonnelasco01@gmail.com>
2021-11-30 21:30:23 +0800

    description

--- program 1.py
+++ program 1.py
@@ -6,8 +6,10 @@
 sentence = input("Enter a Sentence here: ")
 space_count = 0
 vowels_count = 0
+consonants_count = 0
 
 list1 = ["a","e","i","o","u","A","E","I","O","U"]
+list_2 = ["B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z","b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
 
 for character in sentence:
     if character == " ":
@@ -18,6 +20,11 @@ for character in sentence:
     if character in list1:
         vowels_count = vowels_count + 1
 
+for character in sentence:
+    if character in list_2:
+        consonants_count = consonants_count + 1
+
 print("You entered:",sentence)
 print("Number of words:", number_of_words)
 print("Number of vowels:", vowels_count)
+print("Number of consonants:", consonants_count)

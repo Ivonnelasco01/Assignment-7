d9c67508ed2c8b9489cd504a7bd1a860fcbb31b7
Jaren <ivonnelasco01@gmail.com>
2021-11-30 20:58:04 +0800

    description

--- program 1.py
+++ program 1.py
@@ -5,11 +5,19 @@
 
 sentence = input("Enter a Sentence here: ")
 space_count = 0
+vowels_count = 0
+
+list1 = ["a","e","i","o","u","A","E","I","O","U"]
 
 for character in sentence:
     if character == " ":
         space_count = space_count + 1
 number_of_words = space_count + 1
 
-print("You entered: ",sentence)
-print("number of words: ", number_of_words)
+for character in sentence:
+    if character in list1:
+        vowels_count = vowels_count + 1
+
+print("You entered:",sentence)
+print("Number of words:", number_of_words)
+print("Number of vowels:", vowels_count)

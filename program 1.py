606e05de3d05dd15a618040781a2abfab18b793e
Jaren <ivonnelasco01@gmail.com>
2021-11-30 20:45:17 +0800

    description

--- program 1.py
+++ program 1.py
@@ -2,5 +2,14 @@
 #count words
 #count vowels
 #count consonants
+
 sentence = input("Enter a Sentence here: ")
-print(sentence)
+space_count = 0
+
+for character in sentence:
+    if character == " ":
+        space_count = space_count + 1
+number_of_words = space_count + 1
+
+print("You entered: ",sentence)
+print("number of words: ", number_of_words)

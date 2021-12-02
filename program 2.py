c88243f2afc96571f9a00336a6879e763a902cf0
Jaren <ivonnelasco01@gmail.com>
2021-12-02 14:46:03 +0800

    description

--- program 2.py
+++ program 2.py
@@ -11,6 +11,16 @@ print("b. Have at least one capital letter")
 print("c. Have at least one number")
 print("d. Have at least one special char")
 print(" ")
+
 user_password = input("Enter your password here: ")
+password_validator = True
+
+while password_validator:
+    # a. Greater than 15 letters
+    if (len(user_password)<15):
+        print("Password must contain greater than 15 character")
+        break
+    
+
 
 

bb013d5d9aba384864af9e7b31bf2ab324589e1e
Jaren <ivonnelasco01@gmail.com>
2021-12-02 14:29:23 +0800

    description

---
+++ program 2.py
@@ -0,0 +1,16 @@
+# enter password
+#a. Greater than 15 letters
+#b. Have at least one capital letter
+#c. Have at least one number
+#d. Have at least one special char
+import re
+print(" Password validator ")
+print("The password is valid if all criteria are met:")
+print("a. Greater than 15 letters")
+print("b. Have at least one capital letter")
+print("c. Have at least one number")
+print("d. Have at least one special char")
+print(" ")
+user_password = input("Enter your password here: ")
+
+

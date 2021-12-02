9bd0dab16babe7a463c419f24ebf3ec8c95764f6
Jaren <ivonnelasco01@gmail.com>
2021-12-03 01:12:06 +0800

    description

--- program 2.py
+++ program 2.py
@@ -5,7 +5,6 @@
 #c. Have at least one number
 #d. Have at least one special char
 
-import re
 
 print(" Password validator ")
 print("The password is valid if all criteria are met:")
@@ -17,16 +16,23 @@ print(" ")
 
 password = input("Enter your password here: ")
 uppercase = False
+number = False
 
 for char in password:
     if char in "ABCDEFGHIJKLMNOPQRSTUVWXXYZ":
         uppercase = True
+    elif char in "0123456789":
+        number = True
+
+if len(password) < 15:
+    print("a. Greater than 15 letters")
 
 if uppercase == False:
     print("b. Have at least one capital letter")
 
-if len(password) < 15:
-    print("a. Greater than 15 letters")
+if number == False:
+    print("c. Have at least one number")
+
 
 
 

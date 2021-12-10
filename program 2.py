485f1f1e1f114e97c540b404c43872ee917ea1ab
ivonne.lasco <92631714+Ivonnelasco01@users.noreply.github.com>
2021-12-10 21:41:55 +0800

    Update program 2.py

--- program 2.py
+++ program 2.py
@@ -1,4 +1,3 @@
-
 # enter password
 #a. Greater than 15 letters
 #b. Have at least one capital letter
@@ -16,30 +15,18 @@ print("c. Have at least one number")
 print("d. Have at least one special char")
 print(" ")
 
-character = 0
-uppercase = 0
-number = 0
-special = 0
-
-while True:
-    password = input("Enter your password here: ")
-    for char in password:
-        if len(password) > 15:
-            character += 1
-        if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-            uppercase += 1
-        if char in "0123456789":
-            number += 1
-        if char in "!@#$%^&*()_+-=,./;'[]<>?:{}\|":
-            special += 1
-    if character == 0:
-        print("Password must be greater than 15 characters")
-    elif uppercase == 0:
-        print("Password must have at least one capital letter")
-    elif number == 0:
-        print("Have at least one number")
-    elif special == 0:
-        print("Have at least one special character")
-    else:
-        print("Password is valid")
-        break
+def password_validator():
+    while True:
+        password = input("Please enter you password here: ")
+        if len(password) < 15:
+            print("Password must atleast 15 characters, one capital letter, one number, and one special character ")
+        elif not "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+            print("Password must atleast 15 characters, one capital letter, one number, and one special character ")
+        elif not "0123456789":
+            print("Password must atleast 15 characters, one capital letter, one number, and one special character ")
+        elif not "!@#$%^&*()_+-=,./;'[]<>?:{}\|":
+            print("Password must atleast 15 characters, one capital letter, one number, and one special character ")
+        else:
+            print("Your password is valid")
+            break
+password_validator()
